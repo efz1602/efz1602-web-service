8c30c9a2fdfe770f56414210cce8ac088d04eadf
Kyle Yan <kyleyan@cs.washington.edu>
2020-07-15 01:58:00 -0700

    basic user sign up, sign in

--- ews-api/config/routes.rb
+++ ews-api/config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_for :users,
+                 path: '',
+                 path_names: {
+                   sign_in: 'login',
+                   sign_out: 'logout',
+                   registration: 'signup'
+                 },
+                 controllers: {
+                   sessions: 'api/v1/sessions',
+                   registrations: 'api/v1/registrations'
+                 }
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
